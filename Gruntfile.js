ad0cb6983c82e9cbda1b99f2ba7df9ed87460955
Catherine Farman <farman@happycog.com>
2014-03-28 17:18:16 -0400

    Fix Gruntfile tasks and file paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,12 +14,12 @@ module.exports = function(grunt){
           sourcemap: true
         },
         files: {
-          'a/c/screen.css' : 'a/c/screen.scss'
+          'css/styles.css' : 'scss/styles.scss'
         }
       },
       prod: {
         files: {
-          'a/c/screen.css' : 'a/c/screen.scss'
+          'css/styles.css' : 'scss/styles.scss'
         }
       }
     },
@@ -31,13 +31,13 @@ module.exports = function(grunt){
         },
         files: {
           // Where to combine and minify JS files, followed by list of which files to include and exclude
-          'a/j/script.min.js' : ['a/j/plugins/*.js', 'a/j/script.js', '!a/j/plugins/modernizr.js']
+          'js/script.min.js' : ['js/plugins/*.js', 'js/script.js', '!js/plugins/modernizr.js']
         }
       },
       prod: {
         files: {
           // Where to combine and minify JS files, followed by list of which files to include and exclude
-          'a/j/script.min.js' : ['a/j/plugins/*.js', 'a/j/script.js', '!a/j/plugins/modernizr.js', '!a/j/plugins/livereload.js']
+          'js/script.min.js' : ['js/plugins/*.js', 'js/script.js', '!js/plugins/modernizr.js', '!js/plugins/livereload.js']
         }
       }
     },
@@ -48,11 +48,11 @@ module.exports = function(grunt){
         livereload: true
       },
       css: {
-        files: ['a/c/**/*.scss'],
-        tasks: ['compass'] // Compile with Compass when Sass changes are saved
+        files: ['scss/*.scss'],
+        tasks: ['sass']
       },
       js: {
-        files: ['a/j/**/*.js', '!a/j/script.min.js'], // Watch for changes in JS files except for script.min.js to avoid reload loops
+        files: ['js/**/*.js', '!js/script.min.js'], // Watch for changes in JS files except for script.min.js to avoid reload loops
         tasks: ['uglify:dev']
       }
 		}
